7bc214d64a92a0553ad81e3ed5ba9dc206a338df
Nate <fatenuller@gmail.com>
2015-12-10 21:27:32 +0000

    Automatically committed using Kill the Mutants

---
+++ example3/1449782845/Tests.java
@@ -0,0 +1,50 @@
+package example3;
+
+/*
+ * Special thanks to
+ * http://users.csc.calpoly.edu/~djanzen/tdl/tddintro/helloworld/
+ * for getting us started.
+ */
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class Tests
+{
+
+	private Snippet h;
+
+	@Before
+	public void setUp() throws Exception
+	{
+		h = new Snippet();
+	}
+
+	@Test
+	public void test() {
+	    int[] arr0 = {};
+	    int[] arr1 = {1};
+	    int[] arr2 = {1, 2, 3, 4};
+	    int[] arr3 = { -5, -10, 5, 8, 99};
+		int[] arr4 = {2,4,5,3,1,7,9,10};
+		
+		assertTrue(isSorted(h.doInsertionSort(arr0)));
+		assertTrue(isSorted(h.doInsertionSort(arr1)));
+		assertTrue(isSorted(h.doInsertionSort(arr2)));
+		assertTrue(isSorted(h.doInsertionSort(arr3)));
+		assertTrue(isSorted(h.doInsertionSort(arr4)));
+	}
+	
+	// assuming ascending order
+	private boolean isSorted(int[] arr) {
+	    for (int i = 0; i < arr.length - 1; i++) {
+	        if (arr[i] > arr[i + 1])
+	            return false;
+	    }
+	    return true;
+	}
+
+}
