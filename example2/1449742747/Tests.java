a94d8f07e54458b1b2af630b5f2b87558ac1df92
Nate <fatenuller@gmail.com>
2015-12-10 10:19:13 +0000

    Automatically committed using Kill the Mutants

---
+++ example2/1449742747/Tests.java
@@ -0,0 +1,32 @@
+package example2;
+
+/*
+ * Special thanks to
+ * http://users.csc.calpoly.edu/~djanzen/tdl/tddintro/helloworld/
+ * for getting us started.
+ */
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class Tests
+{
+
+	private Snippet h;
+
+	@Before
+	public void setUp() throws Exception
+	{
+		h = new Snippet();
+	}
+
+	@Test
+	public void testConcat() {
+	    assertEquals(h.concatenate("cat","dog"), "catdog");
+	    assertEquals(h.concatenate("","hi"), "hi");
+	}
+
+}
